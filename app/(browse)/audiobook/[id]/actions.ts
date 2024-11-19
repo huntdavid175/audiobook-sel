618d375d5bb398501042ab0a4421930a88c775c1
huntdavid175 <huntdavid175@gmail.com>
2024-11-19 09:04:06 -0400

    checkout completed

--- app/(browse)/audiobook/[id]/actions.ts
+++ app/(browse)/audiobook/[id]/actions.ts
@@ -2,6 +2,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export const addToCart = async (formData: FormData) => {
   const supabase = createClient();
@@ -39,3 +42,57 @@ export const deleteCartItem = async (cartId: string) => {
     console.error(error);
   }
 };
+
+// export const loadProductsToStripe = async () => {
+//   const supabase = createClient();
+//   const { data: products, error } = await supabase.from("books").select("*");
+
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+
+//   if (products) {
+//     for (const product of products) {
+//       try {
+//         // Create Stripe product
+//         const stripeProduct = await stripe.products.create({
+//           name: product.title,
+//           description: product.description,
+//           images: [product.image],
+//           active: true,
+//           id: product.id, // Custom product ID from your database
+//         });
+//         console.log(`Stripe Product Created: ${stripeProduct.id}`);
+
+//         // Create Stripe price
+//         const stripePrice = await stripe.prices.create({
+//           product: stripeProduct.id,
+//           unit_amount: product.price * 100, // Price in cents
+//           currency: "cad",
+//         });
+//         console.log(`Stripe Price Created: ${stripePrice.id}`);
+
+//         // Update your database with the Stripe price_id
+//         const { error: updateError } = await supabase
+//           .from("books")
+//           .update({ stripe_price_id: stripePrice.id })
+//           .eq("id", product.id);
+
+//         if (updateError) {
+//           console.error(
+//             `Error updating price_id for product ${product.title}:`,
+//             updateError
+//           );
+//         } else {
+//           console.log(`Updated product ${product.title} with Stripe price_id.`);
+//         }
+//       } catch (error) {
+//         console.error(
+//           `Error creating product or price for ${product.title}:`,
+//           error
+//         );
+//       }
+//     }
+//   }
+// };
--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -1,29 +1,64 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: NextRequest) {
-  try {
-    // Create Checkout Sessions from body params.
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: "price_1QItBlDkPExXjtPNEOuqV9KH",
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `${req.nextUrl.origin}/?success=true`,
-      cancel_url: `${req.nextUrl.origin}/?canceled=true`,
-    });
+  const supabase = createClient();
 
-    // Redirect to Stripe's checkout session
-    return NextResponse.redirect(session.url!, { status: 303 });
-  } catch (err: any) {
-    // If an error occurs, return an error response
-    return new NextResponse(err.message, { status: err.statusCode || 500 });
+  const { data: cart, error: cartError } = await supabase
+    .from("cart")
+    .select("*");
+
+  if (cartError) {
+    console.error(cartError);
+  }
+
+  if (cart) {
+    const { data: price_ids, error: productsError } = await supabase
+      .from("books")
+      .select("stripe_price_id")
+      .in(
+        "id",
+        cart.map((item) => item.product_id)
+      );
+
+    const { error: libraryError } = await supabase
+      .from("library")
+      .insert(cart.map((item) => ({ book_id: item.product_id })));
+
+    if (productsError) {
+      console.error(productsError);
+    }
+
+    if (libraryError) {
+      console.log(libraryError);
+    }
+
+    if (price_ids) {
+      const lineItems = price_ids.map((item) => {
+        return {
+          price: item.stripe_price_id,
+          quantity: 1,
+        };
+      });
+      try {
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          line_items: lineItems,
+          mode: "payment",
+          success_url: `${req.nextUrl.origin}/?success=true`,
+          cancel_url: `${req.nextUrl.origin}/?canceled=true`,
+        });
+
+        // Redirect to Stripe's checkout session
+        return NextResponse.redirect(session.url!, { status: 303 });
+      } catch (err: any) {
+        // If an error occurs, return an error response
+        return new NextResponse(err.message, { status: err.statusCode || 500 });
+      }
+    }
   }
 }
 
